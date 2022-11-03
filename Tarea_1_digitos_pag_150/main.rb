505f6a43c783ca4f4a87ea947c24b24ecefacee8
JohanCampos <43768539+JohanCampos@users.noreply.github.com>
2022-11-03 11:46:07 -0600

    Actualizar práctica de lexico

--- Tarea_1_digitos_pag_150/main.rb
+++ Tarea_1_digitos_pag_150/main.rb
@@ -1,61 +1,51 @@
 
 def initializate
     #cada estado guarda el estado ligado dependiendo de su transición
-    #[digito,-,.,otro,+,E]; el estado 0 es vacío; f es estado de aceptación
-    tabla = [[2,1,0,0,0,0],[2,0,0,0,0,0],[2,3,4,3,3,3],["f","f","f","f","f","f"],
-    [5,0,0,0,0,0],[5,6,6,6,6,7],["f","f","f","f","f","f"],[9,8,0,0,8,0],
-    [9,0,0,0,0,0],[9,10,10,10,10,10],["f","f","f","f","f","f"]]
+    #[digito,-,.,E,+,otro];
+    #Estados de aceptación: 3,5,8,9; estado de aceptación que indica que no es dígito: 9
+    tabla = [[3,1,9,9,2,9],[3,9,9,9,9,9],[3,9,9,9,9,9],[3,9,4,9,9,9],[5,9,9,9,9,9],
+    [5,9,9,6,9,9],[8,7,9,9,7,9],[8,9,9,9,9,9],[8,9,9,9,9,9],[9,9,9,9,9,9]]
     return tabla
 end
 
 def mover(caracter,posicion,tabla)
     #preguntar si es un dígito
-    if Integer === caracter
-        elemento = 1
+    if caracter == "0" || caracter == "1" || caracter == "2" || caracter == "3" || caracter == "4" || caracter == "5" || caracter == "6" || caracter == "7" || caracter =="8" || caracter == "9"
+        elemento = 0
     #preguntar si es un -
     elsif caracter == "-"
-        elemento = 2
+        elemento = 1
     #preguntar si es un .
     elsif caracter == "."
-        elemento = 3
+        elemento = 2
     #preguntar si es un +
     elsif caracter == "+"
         elemento = 4
     #preguntar si es un E    
     elsif caracter == "E"
-        elemento = 5
+        elemento = 3
     #Caso de otro
     else
-        elemento = 6
+        elemento = 5
     end
     estadoInicial = tabla[posicion]
     estadoFinal = estadoInicial[elemento]
-    if estadoFinal == 0
-        return false
-    elsif estadoFinal == "f"
-        return true
-    else
-        return estadoFinal
-    end
     return estadoFinal        
 end
 
 tablaAsignacion = initializate
 puts("Ingrese su cadena a analizar. \n")
 cadena = gets.chomp
-cadena = cadena.upcase
+cadenaDividida = cadena.split("")
 posicion = 0
-condicion = false
-while condicion == false
-    cadena.each do |caracter|
-        posicion = mover(caracter,posicion)
-        if posicion == false | posicion == true
-            break
-        end
-    end
+for i in (0..(cadenaDividida.length-1))
+    posicion = mover(cadenaDividida[i],posicion,tablaAsignacion)
 end
-if posicion == false
+if posicion == 9
     puts(cadena + " no es un dígito.")
-elsif posicion == true
+elsif posicion == 3 || posicion == 5 || posicion == 8
     puts(cadena + " es un dígito.")
+else
+    puts("La cadena "+ cadena + " está incompleta.")
+end 
 gets.chomp
